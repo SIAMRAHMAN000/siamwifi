e64f7541a2dcaf71b1405447228b0b46b74359c7
SIAM RAHMAN <118335510+SIAMRAHMAN000@users.noreply.github.com>
2025-09-08 02:54:48 +0600

    Delete installer.sh

--- installer.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-pkg install -y root-repo 
-pkg install -y git tsu python wpa-supplicant pixiewps iw
-
-git clone --depth 1 https://github.com/SIAMRAHMAN000/siamwifi siamwifi
-
-chmod +x siamwifi/siam.py
